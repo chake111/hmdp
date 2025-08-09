6a6a68ac71ae1e350f86d8ca716e32e6465ed54c
chake111 <qpalsd@foxmail.com>
2025-08-09 12:05:39 +0800

    feat: 添加关注和检查是否关注功能

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,12 +1,14 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 虎哥
@@ -14,6 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/follow")
+@RequiredArgsConstructor
 public class FollowController {
 
+    private final IFollowService followService;
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long id,
+                         @PathVariable("isFollow") boolean isFollow) {
+        return followService.follow(id, isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long id) {
+        return followService.isFollow(id);
+    }
 }
